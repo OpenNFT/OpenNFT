eae759e340563ead0b0196666eb318cc38d9eb37
tiborauer <tibor.auer@gmail.com>
2019-02-03 04:13:31 +0000

    UPDATE: UDP

--- tests/testUDPReceiver.py
+++ tests/testUDPReceiver.py
@@ -18,13 +18,14 @@ UDP_CONTROL_CHAR = '#'
 receiver = Udp(IP=UDP_IP,port=UDP_PORT,controlChar=UDP_CONTROL_CHAR)
 
 receiver.ConnectForReceiving()
+receiver.sendTimeStamp = True
 
 n = 0
 while receiver.isOpen:
     data = receiver.ReceiveData(n=1,dtype='float')
     n += 1
-    if n == 1: receiver.ResetClock()
-    if len(data): receiver.Log('volume #{:3d} feedback: {:2.3f}'.format(n,data[0]))
+    # if n == 1: receiver.ResetClock()
+    if len(data) > 1: receiver.Log('volume #{:3d} feedback: {} - {:2.3f}'.format(n,data[0],data[1]))
     else: receiver.Log('volume #{:3d} no data!'.format(n))
 
 receiver.Close()
