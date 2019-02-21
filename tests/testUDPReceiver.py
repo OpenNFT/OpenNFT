35f84161edc54a9bf08831e3b227dbe551c3f553
tiborauer <tibor.auer@gmail.com>
2019-02-21 20:13:53 +0000

    UPDATE: testUDP

--- tests/testUDPReceiver.py
+++ tests/testUDPReceiver.py
@@ -23,15 +23,15 @@ receiver.sendTimeStamp = True
 n = 0
 cond = 'test'
 while receiver.isOpen:
-    try:
-        data = receiver.ReceiveData(n=1,dtype='float')
-    except ValueError as e:
-        cond = str(e).split()[-1]
-        continue
+    data = receiver.ReceiveData(n=1,dtype='float')
+    if len(data) > 1:
+        if type(data[1]) == str:
+            cond = data[1]
+            continue
 
-    n += 1
-    # if n == 1: receiver.ResetClock()
-    if len(data) > 1: receiver.Log('volume #{:3d}, condittion: {}, feedback: {} - {}'.format(n,cond,data[0],data[1]))
+        n += 1
+        # if n == 1: receiver.ResetClock()
+        receiver.Log('volume #{:3d}, condittion: {}, feedback: {} - {}'.format(n,cond,data[0],data[1]))
     elif receiver.isOpen: receiver.Log('volume #{:3d} no data!'.format(n))
 
 receiver.Close()
