40bbe2c2261cb4593a48df67b3cd5b7198d55de0
tiborauer <tibor.auer@gmail.com>
2019-03-09 23:50:04 +0000

    UPDATE: clean code (config, nfbCalc)

--- config.py
+++ config.py
@@ -27,25 +27,6 @@ IMAPH_FILES_EXTENSION = '.img'
 # MRI triggering is required
 USE_MRPULSE = False
 
-# receive data via TCP
-USE_TCP_DATA = False
-if USE_TCP_DATA:
-    # UDP sender settings
-    TCP_DATA_IP = "127.0.0.1" # localhost
-    TCP_DATA_PORT = 1234
-
-# export feedback via UDP
-USE_UDP_FEEDBACK = True
-if USE_UDP_FEEDBACK:
-    # UDP sender settings
-    UDP_FEEDBACK_IP = "127.0.0.1" # localhost
-    UDP_FEEDBACK_PORT = 1234
-    UDP_FEEDBACK_CONTROLCHAR = '#\n'
-    UDP_SEND_CONDITION = True
-
-# use PTB by default, the check box on the GUI can be unselected during the Review of the Parameters
-USE_PTB = False
-
 # Time between two iterations
 MAIN_LOOP_CALL_PERIOD = 30  # ms
 
