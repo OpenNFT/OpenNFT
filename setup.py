f9ad9250724f1f9af50d1f94b51193176d46c2bc
Eugene Prilepin <esp.home@gmail.com>
2019-04-13 17:10:12 +0300

    Move configs directory to the package

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='OpenNFT',
+    version='0.1.0',
+    packages=['opennft'],
+    url='http://www.OpenNFT.org',
+    license='GPL-3.0',
+    author='OpenNFT Team',
+    author_email='opennft@gmail.com',
+    description='An open-source Python/Matlab framework for real-time fMRI neurofeedback training'
+)
