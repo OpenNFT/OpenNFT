013fb1eb238370238ad09ead6de3f92adb5a1e4d
Eugene Prilepin <esp.home@gmail.com>
2019-04-14 18:02:23 +0300

    Add copyright docstring

--- opennft/__main__.py
+++ opennft/__main__.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+
+__________________________________________________________________________
+Copyright (C) 2016-2019 OpenNFT.org
+
+"""
+
 from opennft.opennft import main
 
 if __name__ == '__main__':
