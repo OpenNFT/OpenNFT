89f1fce580b41dc245c2ddfa157a9bb80dd3a7b9
Eugene Prilepin <esp.home@gmail.com>
2019-06-11 04:26:19 +0300

    Change log format

--- opennft/_logging.py
+++ opennft/_logging.py
@@ -8,5 +8,5 @@ def logging_setup():
     logger.remove()
     logger.add(
         sys.stdout,
-        format='<g>{time:YYYY-MM-DD hh:mm:ss}</g> | <level>{level:9}</level> | <c>{module}</c>:<c>{function}</c> - <b>{message}</b>'
+        format='<g>{time:YYYY-MM-DD hh:mm:ss}</g> | <level>{level:9}</level> | <c>{module}</c>:<c>{function}</c>:<le>{line}</le> - <b>{message}</b>'
     )
