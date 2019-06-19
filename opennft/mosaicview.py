89be6d187843c1794154d0355bd5c00e9ddef467
Eugene Prilepin <esp.home@gmail.com>
2019-06-19 23:15:18 +0300

    Refactoring: add RgbaStatsMap class for creating RGBA stats map image

--- opennft/mosaicview.py
+++ opennft/mosaicview.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import typing as t
 from PyQt5 import QtWidgets
 
 import pyqtgraph as pg
 import numpy as np
 
 from opennft import pgext
-from opennft import colormap
 
 
 class MosaicImageViewWidget(QtWidgets.QWidget):
@@ -19,12 +17,6 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
 
         self._background_imitem = pg.ImageItem(autoDownsample=True)
         self._stats_map_imitem = pg.ImageItem(autoDownsample=True)
-
-        self._stats_map_image = None
-        self._thr_min = None
-        self._thr_max = None
-        self._cmap = 'hot'
-        self._thr_percentiles = [5., 95.]
 
         viewbox = pgext.ViewBoxWithoutPadding(
             lockAspect=True,
@@ -49,19 +41,8 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
     def set_background_image(self, image: np.ndarray):
         self._background_imitem.setImage(image.T)
 
-    def set_stats_map_image(self, image: t.Optional[np.ndarray], compute_thresholds: bool = True):
-        if image is None:
-            self._stats_map_imitem.clear()
-            self._stats_map_image = None
-            self._thr_min = self._thr_max = None
-            return
-
-        self._stats_map_image = np.ma.masked_equal(image, 0.0)
-
-        if compute_thresholds or self._thr_min is None:
-            self._compute_stats_map_thresholds()
-
-        self._update_stats_map()
+    def set_stats_map_image(self, image: np.ndarray):
+        self._stats_map_imitem.setImage(image.transpose((1, 0, 2)))
 
     def clear(self):
         self._background_imitem.clear()
@@ -69,33 +50,3 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
 
     def clear_stats_map(self):
         self._stats_map_imitem.clear()
-
-    @property
-    def stats_map_min_threshold(self) -> float:
-        return self._thr_min
-
-    @property
-    def stats_map_max_threshold(self) -> float:
-        return self._thr_max
-
-    def set_stats_map_min_threshold(self, thr):
-        self._thr_min = thr
-        self._update_stats_map()
-
-    def set_stats_map_max_threshold(self, thr):
-        self._thr_max = thr
-        self._update_stats_map()
-
-    def _compute_stats_map_thresholds(self):
-        percentiles = np.percentile(self._stats_map_image.compressed(), self._thr_percentiles)
-        self._thr_min = percentiles[0]
-        self._thr_max = percentiles[1]
-
-    def _update_stats_map(self):
-        if self._stats_map_image is None:
-            return
-
-        map_ma = np.ma.masked_outside(self._stats_map_image, self._thr_min, self._thr_max)
-        map_rgba = colormap.map_to_rgba(map_ma, cmap=self._cmap)
-
-        self._stats_map_imitem.setImage(map_rgba.transpose((1, 0, 2)))
