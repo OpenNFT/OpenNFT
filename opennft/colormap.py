d3efe292872828e45f77d1c6eb10f79f63497af5
Eugene Prilepin <esp.home@gmail.com>
2019-06-19 17:39:48 +0300

    Compute thresholds for mosaic stats map

--- opennft/colormap.py
+++ opennft/colormap.py
@@ -7,19 +7,26 @@ from matplotlib import cm
 from matplotlib import colors
 
 
+_DataType = t.Union[np.ndarray, np.ma.MaskedArray]
 _CmapType = t.Union[str, colors.Colormap]
+_NormType = t.Optional[float]
 
 
-def map_to_rgba(data: np.ndarray, cmap: _CmapType):
+def map_to_rgba(data: _DataType, cmap: _CmapType, norm_min: _NormType = None, norm_max: _NormType = None):
     """Mapping data to RGBA using given colormap
 
     Parameters
     ----------
 
-    data: np.ndarray, np.ma.MaskedArray
+    data : np.ndarray, np.ma.MaskedArray
         The 2D-data array (an image of projection for example)
-    cmap: Colormap, str
+    cmap : Colormap, str
         Colormap representation instance or name string
+    norm_min : float, None
+        Minimum value for colormap normalization
+    norm_max : float, None
+        Maximum value for colormap normalization
+
     Returns
     -------
     data: np.ndarray
@@ -29,9 +36,14 @@ def map_to_rgba(data: np.ndarray, cmap: _CmapType):
     if isinstance(cmap, str):
         cmap = cm.get_cmap(cmap)
 
+    if norm_min is None:
+        norm_min = data.min()
+    if norm_max is None:
+        norm_max = data.max()
+
     cmap.set_bad(alpha=0.0)
 
-    normalizer = colors.Normalize(vmin=data.min(), vmax=data.max())
+    normalizer = colors.Normalize(vmin=norm_min, vmax=norm_max)
     mapper = cm.ScalarMappable(norm=normalizer, cmap=cmap)
 
     return mapper.to_rgba(data)
--- opennft/mosaicview.py
+++ opennft/mosaicview.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import typing as t
 from PyQt5 import QtWidgets
 
 import pyqtgraph as pg
@@ -18,6 +19,12 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
 
         self._background_imitem = pg.ImageItem(autoDownsample=True)
         self._stats_map_imitem = pg.ImageItem(autoDownsample=True)
+
+        self._stats_map_image = None
+        self._thr_min = None
+        self._thr_max = None
+        self._cmap = 'hot'
+        self._thr_percentiles = [5., 95.]
 
         viewbox = pgext.ViewBoxWithoutPadding(
             lockAspect=True,
@@ -39,19 +46,22 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
 
         self.setLayout(self._layout)
 
-    def set_background_image(self, image):
+    def set_background_image(self, image: np.ndarray):
         self._background_imitem.setImage(image.T)
 
-    def set_stats_map_image(self, image):
+    def set_stats_map_image(self, image: t.Optional[np.ndarray], compute_thresholds: bool = True):
         if image is None:
             self._stats_map_imitem.clear()
+            self._stats_map_image = None
+            self._thr_min = self._thr_max = None
             return
 
-        # TODO: use colormap and thresholds from app settings
-        map_ma = np.ma.masked_less(image, 0.1)
-        map_rgba = colormap.map_to_rgba(map_ma, cmap='hot')
+        self._stats_map_image: np.ma.MaskedArray = np.ma.masked_equal(image, 0.0)
 
-        self._stats_map_imitem.setImage(map_rgba.transpose((1, 0, 2)))
+        if compute_thresholds or self._thr_min is None:
+            self._compute_stats_map_thresholds()
+
+        self._update_stats_map()
 
     def clear(self):
         self._background_imitem.clear()
@@ -59,3 +69,33 @@ class MosaicImageViewWidget(QtWidgets.QWidget):
 
     def clear_stats_map(self):
         self._stats_map_imitem.clear()
+
+    @property
+    def stats_map_min_threshold(self) -> float:
+        return self._thr_min
+
+    @property
+    def stats_map_max_threshold(self) -> float:
+        return self._thr_max
+
+    def set_stats_map_min_threshold(self, thr):
+        self._thr_min = thr
+        self._update_stats_map()
+
+    def set_stats_map_max_threshold(self, thr):
+        self._thr_max = thr
+        self._update_stats_map()
+
+    def _compute_stats_map_thresholds(self):
+        percentiles = np.percentile(self._stats_map_image.compressed(), self._thr_percentiles)
+        self._thr_min = percentiles[0]
+        self._thr_max = percentiles[1]
+
+    def _update_stats_map(self):
+        if self._stats_map_image is None:
+            return
+
+        map_ma = np.ma.masked_outside(self._stats_map_image, self._thr_min, self._thr_max)
+        map_rgba = colormap.map_to_rgba(map_ma, cmap=self._cmap)
+
+        self._stats_map_imitem.setImage(map_rgba.transpose((1, 0, 2)))
