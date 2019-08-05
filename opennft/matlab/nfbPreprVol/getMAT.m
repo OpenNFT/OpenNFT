6a88bafee0a0e901c13cd2b19ca3328d22edebbe
Szasz <MFTL003@live.rhul.ac.uk>
2019-08-05 15:23:34 +0100

    UPDATE: allow enabling getMAT

--- opennft/matlab/nfbPreprVol/getMAT.m
+++ opennft/matlab/nfbPreprVol/getMAT.m
@@ -36,7 +36,7 @@ vox    = [hdr.PixelSpacing; hdr.SpacingBetweenSlices];
 pos    = hdr.ImagePositionPatient;
 orient = reshape(hdr.ImageOrientationPatient,[3 2]);
 orient(:,3) = null(orient');
-if det(orient) < 0, orient(:,3) = -orient(:,3); end;
+if det(orient) < 0, orient(:,3) = -orient(:,3); end
 
 % The image position vector is not correct. In dicom this vector points to
 % the upper left corner of the image. Perhaps it is unlucky that this is
@@ -44,15 +44,14 @@ if det(orient) < 0, orient(:,3) = -orient(:,3); end;
 % of the slice (keep in mind: upper left slice) with the enlarged FoV.
 dicom_to_patient = [orient*diag(vox) pos ; 0 0 0 1];
 truepos          = dicom_to_patient * ...
-                        [(double([hdr.Width hdr.Height])-dim(1:2))/2 0 1]';
+                        [([hdr.Columns hdr.Rows]-dim(1:2))/2 0 1]';
 dicom_to_patient = [orient*diag(vox) truepos(1:3) ; 0 0 0 1];
 patient_to_tal   = diag([-1 -1 1 1]);
 mat              = patient_to_tal * dicom_to_patient * analyze_to_dicom;
 
 % Maybe flip the image depending on SliceNormalVector from 0029,1010
 %-------------------------------------------------------------------
-% SliceNormalVector = read_SliceNormalVector(hdr);
-% if det([reshape(hdr.ImageOrientationPatient,[3 2]) SliceNormalVector(:)])<0;
-%     volume = volume(:,:,end:-1:1);
-%     mat    = mat*[eye(3) [0 0 -(dim(3)-1)]'; 0 0 0 1];
-% end;
+SliceNormalVector = str2num([hdr.CSAImageHeaderInfo(cellfun(@(x) strcmpi(x,'SliceNormalVector'),{hdr.CSAImageHeaderInfo.name})).item.val]);
+if det([reshape(hdr.ImageOrientationPatient,[3 2]) SliceNormalVector(:)])<0
+    mat    = mat*[eye(3) [0 0 -(dim(3)-1)]'; 0 0 0 1];
+end
