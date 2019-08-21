9a1a0161b9fb2c9196ad54e76653cfae158574ed
tiborauer <tibor.auer@gmail.com>
2019-08-21 15:46:19 +0100

    UPDATE: implicit brainmask on unthresholded statmap

--- opennft/matlab/nfbPreprVol/iGlmVol.m
+++ opennft/matlab/nfbPreprVol/iGlmVol.m
@@ -86,9 +86,8 @@ iglmActVox.neg = find(tn.neg > tTh(n));
 % statistical image masking, as set by SPM structure, see
 % setupProcParams.m
 recTh = recTh + spmMaskTh(n) * spmMaskTh(n); % cumulative sum of squares
-spmActVox = find(sigma2n > recTh);
+spmActVox = sigma2n > recTh;
 
 % intersect of iGLM and SPM indexes
-idxActVox.pos = intersect(iglmActVox.pos, spmActVox);
-idxActVox.neg = intersect(iglmActVox.neg, spmActVox);
-clear iglmActVox spmActVox
+tn = tn .* spmActVox; 
+idxActVox = intersect(iglmActVox, find(spmActVox));
