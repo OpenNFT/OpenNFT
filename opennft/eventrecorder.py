dffc00839f27c94e5e2ef9f55ef2a412b4a0665d
tiborauer <tibor.auer@gmail.com>
2020-01-03 19:59:22 +0100

    UPDATE: event time rather than event number

--- opennft/eventrecorder.py
+++ opennft/eventrecorder.py
@@ -86,7 +86,7 @@ class EventRecorder(object):
     def getLastEvent(self, iteration=None):
         if iteration is None:
             iteration = [iteration for iteration, item in enumerate(self.records) if any(item != 0)][-1]
-        return [index for index, item in enumerate(self.records[iteration]) if item != 0][-1]
+        return [index for index, item in enumerate(self.records[iteration]) if item == max(self.records[iteration])][-1]
 
     # --------------------------------------------------------------------------
     def savetxt(self, filename):
