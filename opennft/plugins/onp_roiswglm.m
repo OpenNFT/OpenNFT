a3ea0233a4c05e6d3b361efd6b2be0c9b713f1f9
tiborauer <tibor.auer@gmail.com>
2020-09-04 14:14:22 +0100

    PLUGIN: ROI SW-GLM

---
+++ opennft/plugins/onp_roiswglm.m
@@ -0,0 +1,123 @@
+function betaRoi = onp_roiswglm
+
+P = evalin('base', 'P');
+mainLoopData = evalin('base', 'mainLoopData');
+
+betaRoi = zeros(numel(P.ProtNF),P.NrROIs); % default value indicating no analysis
+
+if ~isfield(mainLoopData,'indVolNorm') % init
+    % load original SPM
+    SPMSpec = load(fullfile(P.WorkFolder,'Settings','SPM.mat')); SPMSpec = SPMSpec.SPM;
+    
+    % make sure that the first regressor is the regulation and the order of the rest matches the order of CondNames
+    P.CondNames(strcmp(P.Protocol.RegulationName, P.CondNames)) = [];
+    P.CondNames = [{P.Protocol.RegulationName} P.CondNames];
+    [junk,regrInd] = ismember(P.CondNames,cellfun(@(x) x.ConditionName, P.Protocol.Cond, 'UniformOutput', false));
+    
+    % add step-wise regressors for each block
+    SPMSpec.Sess.U = struct('name',{},'ons',{},'dur',{},'P',{}, 'orth',{});
+    if ~isempty(regrInd)
+        for e = 1:numel(regrInd)
+            cond = P.Protocol.Cond{regrInd(e)};
+            if strcmp(cond.ConditionName, P.Protocol.RegulationName)
+                for s = 1:size(cond.OnOffsets,1)
+                    if s > 1, SPMSpec(s) = SPMSpec(s-1); end
+                    SPMSpec(s).Sess.U(end+1) = cond2U(cond,s);
+                end
+            else
+                for s = 1:numel(SPMSpec)
+                    SPMSpec(s).Sess.U(end+1) = cond2U(cond);
+                end
+            end
+        end
+    end
+    
+    % generate xX for each model
+    clear SPM
+    for s = 1:numel(SPMSpec)
+        SPM = spm_fmri_spm_ui(SPMSpec(s));
+        if ~P.iglmAR1
+            spmDesign{s} = SPM.xX.X(:,contains(SPM.xX.name,P.CondName));
+        else
+            spmDesign{s} = arRegr(P.aAR1, SPM.xX.X(:,contains(SPM.xX.name,P.CondName)));
+        end
+    end
+    if exist(fullfile(pwd, 'SPM.mat'),'file'), delete('SPM.mat'); end
+    
+    % save models and block periods
+    switchInds = cellfun(@(x) x(1), P.ProtBAS(1:end-1)); % not for the last baseline
+    runInds = cellfun(@(x) x(1)-1, P.ProtBAS(2:end),'UniformOutput', false); % right before the consecutive baselines
+    iDesign = mutableClass(switchInds);
+    iDesign.addProp('runInds',runInds);
+    iDesign.addProp('spmDesign',spmDesign);
+    
+    assignin('base', 'iDesign', iDesign);
+    
+else % run
+    % load model according to block
+    iDesign = evalin('base', 'iDesign');
+    iDesign.indN = mainLoopData.indVolNorm;
+    if mainLoopData.indVolNorm ~= iDesign.runInds, return; end
+    
+    spmDesign = iDesign.spmDesign;
+    tmp_ind_end = mainLoopData.indVolNorm;
+    regrStep = size(spmDesign,2) + 8; % 6 MC regressors, linear trend, constant
+    tmp_rawTimeSeries = mainLoopData.tmp_rawTimeSeriesAR1;
+    
+    % a simplified version of prepSig 2.2
+    if (tmp_ind_end < regrStep)
+        tmpRegr = ones(tmp_ind_end,1);
+        if P.cglmAR1
+            tmpRegr = arRegr(P.aAR1,tmpRegr);
+        end
+        cX0 = tmpRegr;
+    elseif (tmp_ind_end >= regrStep) && (tmp_ind_end < 2*regrStep)
+        tmpRegr = [ones(tmp_ind_end,1) P.linRegr(1:tmp_ind_end)];
+        if P.cglmAR1
+            tmpRegr = arRegr(P.aAR1, tmpRegr);
+        end
+        cX0 = tmpRegr;
+    elseif (tmp_ind_end >= 2*regrStep) && (tmp_ind_end < 3*regrStep)
+        tmpRegr = [ones(tmp_ind_end,1) P.linRegr(1:tmp_ind_end) ...
+            zscore(P.motCorrParam(1:tmp_ind_end,:))];
+        if P.cglmAR1
+            tmpRegr = arRegr(P.aAR1,tmpRegr);
+        end
+        cX0 = tmpRegr;
+    else
+        % zscore() is cumulative, which limits truly recursive
+        % AR(1) filtering on regressors
+        tmpRegr = [ones(tmp_ind_end,1) P.linRegr(1:tmp_ind_end) ...
+            zscore(P.motCorrParam(1:tmp_ind_end,:))];
+        if P.cglmAR1
+            tmpRegr = arRegr(P.aAR1,tmpRegr);
+        end
+        if ~P.isRestingState
+            cX0 = [spmDesign(1:tmp_ind_end,:) tmpRegr];
+        else
+            cX0 = tmpRegr;
+        end
+    end
+    
+    for indRoi = 1:P.NrROIs
+        betaReg = pinv(cX0) * tmp_rawTimeSeries(indRoi,:)';
+        betaRoi(1:iDesign.stageN,indRoi) = betaReg(1:iDesign.stageN);
+    end
+    
+end
+end
+
+function U = cond2U(cond,t)
+% convert OpenNFT condition to SPM U
+% optional: use t-th trial only
+if nargin == 1, t = 1:size(cond.OnOffsets,1); end
+CondName = cond.ConditionName;
+if numel(t) == 1, CondName = sprintf('%s_%02d',CondName,t); end
+U = struct(...
+    'name',{cellstr(CondName)},...
+    'ons',cond.OnOffsets(t,1),...
+    'dur',(diff(cond.OnOffsets(t,:)')+1)',...
+    'P',struct('name','none'),...
+    'orth',1 ...
+    );
+end
---
+++ opennft/plugins/onp_roiswglm.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+"""
+# Plugins
+Plugins allow flexible modification and execution of OpenNFT without touching the core codebase. Plugins can access data, process them in a specific way, 
+and they can be switched on and off according to the user's need.
+
+Each plugin has to be a subclass of *Process class specified in pyniexp.mlplugins. It has to contain a header in a format of dictionary (called META) with prespecified keys:
+- plugin_name: It is a freeform text which will be displayed in the plugin dialog and in the logs.
+- plugin_time: It is a event timestamp as specified in opennft.eventrecorder. Times, and it determines the execution time of the plugin (so far only t3 is implemented)
+- plugin_init: It is the initialization code of the plugin. "{}" can be used to refer to OpenNFT parameters as specified in the P paremeter dictionary. It can be a list of 
+commands, in which case, the first is run to create the object, and the rest are executed afterwards.
+- plugin_signal: It is an expression returning to logical value, and it speicies the condition when the plugin can be executed.
+- plugin_exec: It is the execution code of the plugin, and it is usually calls the plugin's load_data method to transfer some data to the plugin.
+
+*Process classes pyniexp.mlplugins has an abstract/placeholder method called process, which should be overwritten to specify the operation on the data.
+- the input to the process method of dataProcess (called data) is a one-dimensional numpy array
+- the input to the process method of imageProcess (called image) is a multi-dimensional (usually 3D) numpy array as specified during initialization
+
+# ROI step-wise GLM
+This plugin demonstrates how to add you own approach (this one is a step-wise addition of each block) for ROI analysis.
+
+__________________________________________________________________________
+Copyright (C) 2016-2019 OpenNFT.org
+
+Written by Tibor Auer
+
+"""
+
+from pyniexp.mlplugins import dataProcess, SIG_NOTSTARTED, SIG_RUNNING, SIG_STOPPED, SIG_NEWIMAGE
+from loguru import logger
+from multiprocessing import Value, RawArray
+from numpy import array, meshgrid, savetxt
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from os import path
+
+META = {
+    "plugin_name": "ROI step-wise GLM",
+    "plugin_time": "t4", # according to opennft.eventrecorder.Times
+    "plugin_init": [
+        "ROIswGLM(int({NrROIs}),len({ProtNF}),r'{nfbDataFolder}')",
+        "self.parent.eng.evalin('base','onp_roiswglm')"
+    ],
+    "plugin_signal": "self.parent.eng.evalin('base','isfield(mainLoopData,\\\'tmp_rawTimeSeriesAR1\\\')')",
+    "plugin_exec": "load_data(self.parent.eng.evalin('base','onp_roiswglm'))",
+}
+
+class ROIswGLM(dataProcess):
+    def __init__(self,nROIs,nBlocks,nfbDataFolder):
+        super().__init__(nROIs*nBlocks,autostart=False)
+
+        self.nfbDataFolder = nfbDataFolder
+        self.nROIs = nROIs
+        self.nBlocks = nBlocks
+
+        self.rtdata = RawArray('d',[0]*self.nROIs*self.nBlocks*self.nBlocks)
+        self.nData = Value('i',0)
+
+        self.start_process()
+
+    def process(self,data):
+        if any(array(data) != 0):
+            for r in data:
+                self.rtdata[self.nData.value] = r
+                self.nData.value += 1
+        logger.info(('ROIs: [ ' + '{:.3f} '*len(data) + ']').format(*data))
+
+    def finalize_process(self):
+        dat = array(self.rtdata).reshape(self.nBlocks,self.nROIs,self.nBlocks)
+
+        for b in range(0,self.nBlocks):
+            fname = path.join(path.normpath(self.nfbDataFolder), 'ROIswGLM_{:02d}.txt'.format(b+1))
+            savetxt(fname=fname, X=dat[b,:,0:b+1].transpose(), fmt='%.3f', delimiter=',')
+
+
+        X,Y = meshgrid(self.nBlocks,self.nBlocks) 
+        for r in range(0,self.nROIs):
+            ax = plt.subplot(120+(r+1),projection='3d')
+            ax.plot_surface(X,Y,dat[:,r,:])        
+        plt.show()
