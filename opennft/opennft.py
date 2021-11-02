2e6b54d7bc4be9b4d2188147137f5eaf7a9251ce
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-11-02 11:45:38 +0100

    Flake8 warning: W504 line break after binary operator

--- opennft/opennft.py
+++ opennft/opennft.py
@@ -686,8 +686,8 @@ class OpenNFT(QWidget):
                     # self.udpSender.send_data(self.displayData['instrValue'])
 
         if self.cbUseTCPData.isChecked():
-            fname = str(Path(self.P['WatchFolder']) /
-                                 self.P['FirstFileNameTxt'].replace('Image Series No','ImgSerNr').replace('#','iter').format(**(self.P),iter=self.iteration))
+            fname = str(Path(self.P['WatchFolder'])
+                        / self.P['FirstFileNameTxt'].replace('Image Series No','ImgSerNr').replace('#','iter').format(**(self.P),iter=self.iteration))
         else:
             try:
                 fname = self.files_queue.get_nowait()
