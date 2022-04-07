threshold computation fix

opennft/volviewformation.py
@@ -6,7 +6,7 @@ import pydicom
 from scipy import linalg
 from rtspm import spm_imatrix, spm_matrix, spm_slice_vol
 from opennft.conversions import img2d_vol3d, vol3d_img2d, get_mosaic_dim
-from opennft.mapimagewidget import MapImageThresholdsCalculator, RgbaMapImage
+from opennft.mapimagewidget import MapImageThresholdsCalculator, RgbaMapImage, Thresholds
 
 
 class VolViewFormation(mp.Process):
@@ -102,6 +102,8 @@ class VolViewFormation(mp.Process):
 
                         if self.input_data["auto_thr_pos"]:
                             pos_thr = self.thr_calculator(overlay_img)
+                            if pos_thr.lower < 0:
+                                pos_thr = Thresholds(0, pos_thr.upper)
                             self.output_data["pos_thresholds"] = pos_thr
                         else:
                             pos_thr = self.output_data["pos_thresholds"]
@@ -110,6 +112,8 @@ class VolViewFormation(mp.Process):
                         if self.input_data["is_neg"]:
                             if self.input_data["auto_thr_neg"]:
                                 neg_thr = self.thr_calculator(neg_overlay_img)
+                                if neg_thr.lower < 0:
+                                    neg_thr = Thresholds(0, neg_thr.upper)
                                 self.output_data["neg_thresholds"] = neg_thr
                             else:
                                 neg_thr = self.output_data["neg_thresholds"]
@@ -172,8 +176,13 @@ class VolViewFormation(mp.Process):
                     pos_maps_values = np.array(self.output_data["overlay_t"].ravel(), dtype=np.uint8)
                     pos_maps_values = np.append(pos_maps_values, self.output_data["overlay_c"].ravel())
                     pos_maps_values = np.append(pos_maps_values, self.output_data["overlay_s"].ravel())
-                    pos_thr = self.thr_calculator(pos_maps_values)
-                    self.output_data["pos_thresholds"] = pos_thr
+                    if self.input_data["auto_thr_pos"]:
+                        pos_thr = self.thr_calculator(overlay_img)
+                        if pos_thr.lower < 0:
+                            pos_thr = Thresholds(0, pos_thr.upper)
+                        self.output_data["pos_thresholds"] = pos_thr
+                    else:
+                        pos_thr = self.output_data["pos_thresholds"]
                     self.output_data["overlay_t"] = self.pos_image(self.output_data["overlay_t"], pos_thr, 1.0)
                     self.output_data["overlay_c"] = self.pos_image(self.output_data["overlay_c"], pos_thr, 1.0)
                     self.output_data["overlay_s"] = self.pos_image(self.output_data["overlay_s"], pos_thr, 1.0)
@@ -182,8 +191,13 @@ class VolViewFormation(mp.Process):
                         neg_maps_values = np.array(self.output_data["neg_overlay_t"].ravel(), dtype=np.uint8)
                         neg_maps_values = np.append(neg_maps_values, self.output_data["neg_overlay_c"].ravel())
                         neg_maps_values = np.append(neg_maps_values, self.output_data["neg_overlay_s"].ravel())
-                        neg_thr = self.thr_calculator(neg_maps_values)
-                        self.output_data["neg_thresholds"] = neg_thr
+                        if self.input_data["auto_thr_neg"]:
+                            neg_thr = self.thr_calculator(neg_overlay_img)
+                            if neg_thr.lower < 0:
+                                neg_thr = Thresholds(0, neg_thr.upper)
+                            self.output_data["neg_thresholds"] = neg_thr
+                        else:
+                            neg_thr = self.output_data["neg_thresholds"]
                         self.output_data["neg_overlay_t"] = self.neg_image(self.output_data["neg_overlay_t"],
                                                                            neg_thr, 1.0)
                         self.output_data["neg_overlay_c"] = self.neg_image(self.output_data["neg_overlay_c"],